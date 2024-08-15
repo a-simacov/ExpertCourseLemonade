3b1011e0cccd76c955605c4dbc676581d545e23d
slax <slax.xals@gmail.com>
2024-08-15 14:47:49 +0300

    LG-005 activity

--- app/src/main/java/com/synngate/expertcourselemonade/MainActivity.kt
+++ app/src/main/java/com/synngate/expertcourselemonade/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.synngate.expertcourselemonade.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootLayout)) { v, insets ->
+
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.rootLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val viewModel: GameViewModel = GameViewModel()
+
+        binding.gameImageButton.setOnClickListener {
+            val uiState: GameUiState = viewModel.next()
+            uiState.update(binding = binding)
+        }
+
+        val uiState: GameUiState = viewModel.init()
+        uiState.update(binding = binding)
     }
 }
