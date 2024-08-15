a8a2dc7a9e7d19664e9ade080b6f3ea6efca157b
slax <slax.xals@gmail.com>
2024-08-15 13:51:32 +0300

    LG-003 inner ui classes

---
+++ app/src/androidTest/java/com/synngate/expertcourselemonade/DrawableResMatcher.kt
@@ -0,0 +1,20 @@
+package com.synngate.expertcourselemonade
+
+import android.graphics.Color
+import android.view.View
+import android.widget.ImageButton
+import androidx.annotation.DrawableRes
+import androidx.test.espresso.matcher.BoundedMatcher
+import org.hamcrest.Description
+
+class DrawableResMatcher(@DrawableRes private val drawableRes: Int) :
+    BoundedMatcher<View, ImageButton>(ImageButton::class.java) {
+
+    override fun describeTo(description: Description) {
+        description.appendText("drawable resource for button doesn't match $drawableRes")
+    }
+
+    override fun matchesSafely(item: ImageButton): Boolean {
+        return item.background.constantState == item.context.getDrawable(drawableRes)?.constantState
+    }
+}
---
+++ app/src/androidTest/java/com/synngate/expertcourselemonade/game/TextUi.kt
@@ -0,0 +1,36 @@
+package com.synngate.expertcourselemonade.game
+
+import android.view.View
+import android.widget.TextView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewInteraction
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import com.synngate.expertcourselemonade.R
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.Matcher
+
+class TextUi(
+    textResId: Int,
+    containerIdMatcher: Matcher<View>,
+    containerClassTypeMatcher: Matcher<View>
+) {
+
+    private val interaction: ViewInteraction = onView(
+        allOf(
+            containerIdMatcher,
+            containerClassTypeMatcher,
+            withId(R.id.instructionTextView),
+            withText(textResId),
+            isAssignableFrom(TextView::class.java)
+        )
+    )
+
+    fun assertHasText(textResId: Int) {
+        interaction.check(matches(isDisplayed()))
+            .check(matches(withText(textResId)))
+    }
+}
